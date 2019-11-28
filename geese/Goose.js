a347c86b17d662f89fc1ce5294393a25e22e2f7c
jorgep31415 <32918197+jorgep31415@users.noreply.github.com>
2019-11-28 10:04:04 -0800

    Add some goose character traits

--- geese/Goose.js
+++ geese/Goose.js
@@ -26,75 +26,75 @@ class Goose {
         };
 
         let shape_names = [
-            "head",
-            "left_eyebrow",
-            "right_eyebrow",
-            "left_eye",
-            "right_eye",
-            "top_beak",
-            "bottom_beak",
-            "neck",
-            "left_wing",
-            "body",
-            "right_wing",
-            "left_leg",
-            "right_leg",
-            "left_foot",
-            "right_foot",
+            'head',
+            'left_eyebrow',
+            'right_eyebrow',
+            'left_eye',
+            'right_eye',
+            'top_beak',
+            'bottom_beak',
+            'neck',
+            'left_wing',
+            'body',
+            'right_wing',
+            'left_leg',
+            'right_leg',
+            'left_foot',
+            'right_foot',
         ];
 
         let shapes = [
-            new Subdivision_Sphere(3), // "head"
-            new Cube(), // "left_eyebrow"
-            new Cube(), // "right_eyebrow"
-            new Rounded_Capped_Cylinder(12, 12, .2, .1, [0,1]), // "left_eye"
-            new Rounded_Capped_Cylinder(12, 12, .2, .1, [0,1]), // "right_eye"
-            new Rounded_Cone(12, 12, 1, 2, Math.PI, [0,1]), // "top_beak"
-            new Rounded_Cone(12, 12, 1, 2, Math.PI, [0,1]), // "bottom_beak"
-            new Rounded_Capped_Cylinder(12, 12, .6, 4, [0,1]), // "neck"
-            new Wing(), // "left_wing"
-            new Body(), // "body"
-            new Wing(), // "right_wing"
-            new Rounded_Capped_Cylinder(12, 12, .2, 3.5, [0,1]), // "left_leg"
-            new Rounded_Capped_Cylinder(12, 12, .2, 3.5, [0,1]), // "right_leg"
-            new Foot(), // "left_foot"
-            new Foot(), // "right_foot"
+            new Subdivision_Sphere(3), // 'head'
+            new Cube(), // 'left_eyebrow'
+            new Cube(), // 'right_eyebrow'
+            new Rounded_Capped_Cylinder(12, 12, .2, .1, [0,1]), // 'left_eye'
+            new Rounded_Capped_Cylinder(12, 12, .2, .1, [0,1]), // 'right_eye'
+            new Rounded_Cone(12, 12, 1, 2, Math.PI, [0,1]), // 'top_beak'
+            new Rounded_Cone(12, 12, 1, 2, Math.PI, [0,1]), // 'bottom_beak'
+            new Rounded_Capped_Cylinder(12, 12, .6, 4, [0,1]), // 'neck'
+            new Wing(), // 'left_wing'
+            new Body(), // 'body'
+            new Wing(), // 'right_wing'
+            new Rounded_Capped_Cylinder(12, 12, .2, 3.5, [0,1]), // 'left_leg'
+            new Rounded_Capped_Cylinder(12, 12, .2, 3.5, [0,1]), // 'right_leg'
+            new Foot(), // 'left_foot'
+            new Foot(), // 'right_foot'
         ];
 
         let shape_transforms = [
-            Mat4.identity(), // "head"
-            Mat4.identity().times(Mat4.translation([ 0.5, 0.75,-0.4])).times(Mat4.rotation( Math.PI/6, Vec.of(0,1,0))).times(Mat4.rotation( Math.PI/4, Vec.of(0,0,1))).times(Mat4.rotation(-Math.PI/12, Vec.of(1,0,0))).times(Mat4.scale([ 0.05, 0.04, 0.25])), // "left_eyebrow"
-            Mat4.identity().times(Mat4.translation([ 0.5, 0.75, 0.4])).times(Mat4.rotation(-Math.PI/6, Vec.of(0,1,0))).times(Mat4.rotation( Math.PI/4, Vec.of(0,0,1))).times(Mat4.rotation( Math.PI/12, Vec.of(1,0,0))).times(Mat4.scale([ 0.05, 0.04, 0.25])), // "right_eyebrow"
-            Mat4.identity().times(Mat4.rotation(-Math.PI/3, Vec.of(0,1,0))).times(Mat4.rotation( Math.PI/6, Vec.of(1,0,0)).times(Mat4.translation([ 0, 0,-1]))), // "left_eye"
-            Mat4.identity().times(Mat4.rotation( Math.PI/3, Vec.of(0,1,0))).times(Mat4.rotation(-Math.PI/6, Vec.of(1,0,0)).times(Mat4.translation([ 0, 0, 1]))), // "right_eye"
-            Mat4.identity().times(Mat4.translation([ 0, -0.1, 0])).times(Mat4.scale([ 0.9, 0.7, 0.9])).times(Mat4.rotation( Math.PI/2, Vec.of(0,1,0))), // "top_beak"
-            Mat4.identity().times(Mat4.translation([ 0, -0.1, 0])).times(Mat4.scale([ 0.9, 0.7, 0.9])).times(Mat4.rotation( Math.PI, Vec.of(1,0,0))).times(Mat4.rotation( Math.PI/2, Vec.of(0,1,0))), // "bottom_beak"
-            Mat4.identity().times(Mat4.translation([ -0.4, 0, 0])).times(Mat4.rotation( Math.PI/2, Vec.of(1,0,0))), // "neck"
-            Mat4.identity().times(Mat4.translation([ -7, -7.5, 0])).times(Mat4.translation([ 0, 0, 1])).times(Mat4.scale([ 1.2, 1.2, 1.2])),
-            Mat4.identity().times(Mat4.translation([ -6, -7.5, 1])), // "body"
-            Mat4.identity().times(Mat4.translation([ -7, -7.5, 0])).times(Mat4.scale([ 1, 1,-1])).times(Mat4.translation([ 0, 0, 1])).times(Mat4.scale([ 1.2, 1.2, 1.2])),
-            Mat4.identity().times(Mat4.translation([ -4, -5.75, .75])).times(Mat4.rotation( Math.PI/2, Vec.of( 1, 0, 0))), // "left_leg"
-            Mat4.identity().times(Mat4.translation([ -4, -5.75, -.75])).times(Mat4.rotation( Math.PI/2, Vec.of( 1, 0, 0))), // "right_leg"
-            Mat4.identity().times(Mat4.translation([ -4.25, -9.25, .75])), // "left_foot"
-            Mat4.identity().times(Mat4.translation([ -4.25, -9.25, -.75])), // "right_foot"
+            Mat4.identity(), // 'head'
+            Mat4.identity().times(Mat4.translation([ 0.5, 0.75,-0.4])).times(Mat4.rotation( Math.PI/6, Vec.of(0,1,0))).times(Mat4.rotation( Math.PI/4, Vec.of(0,0,1))).times(Mat4.rotation(-Math.PI/12, Vec.of(1,0,0))).times(Mat4.scale([ 0.05, 0.04, 0.25])), // 'left_eyebrow'
+            Mat4.identity().times(Mat4.translation([ 0.5, 0.75, 0.4])).times(Mat4.rotation(-Math.PI/6, Vec.of(0,1,0))).times(Mat4.rotation( Math.PI/4, Vec.of(0,0,1))).times(Mat4.rotation( Math.PI/12, Vec.of(1,0,0))).times(Mat4.scale([ 0.05, 0.04, 0.25])), // 'right_eyebrow'
+            Mat4.identity().times(Mat4.rotation(-Math.PI/3, Vec.of(0,1,0))).times(Mat4.rotation( Math.PI/6, Vec.of(1,0,0)).times(Mat4.translation([ 0, 0,-1]))), // 'left_eye'
+            Mat4.identity().times(Mat4.rotation( Math.PI/3, Vec.of(0,1,0))).times(Mat4.rotation(-Math.PI/6, Vec.of(1,0,0)).times(Mat4.translation([ 0, 0, 1]))), // 'right_eye'
+            Mat4.identity().times(Mat4.translation([ 0, -0.1, 0])).times(Mat4.scale([ 0.9, 0.7, 0.9])).times(Mat4.rotation( Math.PI/2, Vec.of(0,1,0))), // 'top_beak'
+            Mat4.identity().times(Mat4.translation([ 0, -0.1, 0])).times(Mat4.scale([ 0.9, 0.7, 0.9])).times(Mat4.rotation( Math.PI, Vec.of(1,0,0))).times(Mat4.rotation( Math.PI/2, Vec.of(0,1,0))), // 'bottom_beak'
+            Mat4.identity().times(Mat4.translation([ -0.4, 0, 0])).times(Mat4.rotation( Math.PI/2, Vec.of(1,0,0))), // 'neck'
+            Mat4.identity().times(Mat4.translation([ -7, -7.5, 0])).times(Mat4.translation([ 0, 0, 1])).times(Mat4.scale([ 1.2, 1.2, 1.2])), // 'left_wing'
+            Mat4.identity().times(Mat4.translation([ -6, -7.5, 1])), // 'body'
+            Mat4.identity().times(Mat4.translation([ -7, -7.5, 0])).times(Mat4.scale([ 1, 1,-1])).times(Mat4.translation([ 0, 0, 1])).times(Mat4.scale([ 1.2, 1.2, 1.2])), // 'right_wing'
+            Mat4.identity().times(Mat4.translation([ -4, -5.75, .75])).times(Mat4.rotation( Math.PI/2, Vec.of( 1, 0, 0))), // 'left_leg'
+            Mat4.identity().times(Mat4.translation([ -4, -5.75, -.75])).times(Mat4.rotation( Math.PI/2, Vec.of( 1, 0, 0))), // 'right_leg'
+            Mat4.identity().times(Mat4.translation([ -4.25, -9.25, .75])), // 'left_foot'
+            Mat4.identity().times(Mat4.translation([ -4.25, -9.25, -.75])), // 'right_foot'
         ];
 
         let shape_colors = [
-            "white", // "head"
-            "black", // "left_eyebrow"
-            "black", // "right_eyebrow"
-            "black", // "left_eye"
-            "black", // "right_eye"
-            "orange", // "top_beak"
-            "orange", // "bottom_beak"
-            "white", // "neck"
-            "white", // "left_wing"
-            "white", // "body"
-            "white", // "right_wing"
-            "orange", // "left_leg"
-            "orange", // "right_leg"
-            "orange", // "left_foot"
-            "orange", // "right_foot"
+            'white', // 'head'
+            'black', // 'left_eyebrow'
+            'black', // 'right_eyebrow'
+            'black', // 'left_eye'
+            'black', // 'right_eye'
+            'orange', // 'top_beak'
+            'orange', // 'bottom_beak'
+            'white', // 'neck'
+            'white', // 'left_wing'
+            'white', // 'body'
+            'white', // 'right_wing'
+            'orange', // 'left_leg'
+            'orange', // 'right_leg'
+            'orange', // 'left_foot'
+            'orange', // 'right_foot'
         ];
 
         // shape's key is specifically: shape_name + Goose + goose_id
@@ -111,30 +111,6 @@ class Goose {
             this.colors[shape_names[i] + '_' + this.constructor.name + goose_id] = shape_colors[i];
         }
         
-    }
-    
-    spin_neck = () => {
-        let t_frames = 100;
-        if (this.state.frameNumber == 0)
-            this.state.frameNumber = t_frames;
-
-        let neck_down_transform = Mat4.translation([ -0.4, -4, 0]) // move neck independently
-            .times(Mat4.rotation(-Math.PI / t_frames / 2, Vec.of(0,0,1)))
-            .times(Mat4.translation([0.4,4,0]));
-
-        let body_down_transform = Mat4.translation([-4,-6.75, 0]) // move neck with body
-            .times(Mat4.rotation(-Math.PI / t_frames / 3, Vec.of(0,0,1)))
-            .times(Mat4.translation([4,6.75,0]));
-
-        let neck = 'neck' + '_' + this.constructor.name + this.stats.goose_id;
-
-        this.transforms[neck] = neck_down_transform.times(body_down_transform)
-            .times(this.transforms[neck]);
-
-
-        this.state.frameNumber--;
-        if (this.state.frameNumber == 0)
-            this.state.animating = false;
     }
 
     flap = () => {
--- geese/Lonk.js
+++ geese/Lonk.js
@@ -2,46 +2,156 @@
 // This is a derived class for a Lonk,
 // a longer range character.
 class Lonk extends Goose {
-    constructor() 
-    { super ( LONK_ID );
-        this.stats = {
-            health: 50,
-            attack: 1,
-            defense: 1,
-            range: 1,
-            goose_id: goose_id,
-        };
+    constructor(goose_id) { 
+        super ( goose_id );
 
-        // shape -> shape's constructor
-        this.shapes = {
-            head:       new Subdivision_Sphere( 3 ),
-            left_eye:   new Subdivision_Sphere( 3 ),
-            right_eye:  new Subdivision_Sphere( 3 ),
-            beak:       new Subdivision_Sphere( 3 ),
-            neck:       new Subdivision_Sphere( 3 ),
-            left_wing:  new Subdivision_Sphere( 3 ),
-            body:       new Subdivision_Sphere( 3 ),
-            right_wing: new Subdivision_Sphere( 3 ),
-            left_leg:   new Subdivision_Sphere( 3 ),
-            right_leg:  new Subdivision_Sphere( 3 ),
-            left_foot:  new Subdivision_Sphere( 3 ),
-            right_foot: new Subdivision_Sphere( 3 ),
-        };
+        let tag = '_' + this.constructor.name + this.stats.goose_id;
+        let head = 'head' + tag;
+        let left_eyebrow = 'left_eyebrow' + tag;
+        let right_eyebrow = 'right_eyebrow' + tag;
+        let left_eye = 'left_eye' + tag;
+        let right_eye = 'right_eye' + tag;
+        let top_beak = 'top_beak' + tag;
+        let bottom_beak = 'bottom_beak' + tag;
+
+        //Overwrite these initial transforms.
+        this.transforms[neck]           = Mat4.identity().times(Mat4.translation([ -0.4, 4.0, 0])).times(Mat4.rotation( Math.PI/2, Vec.of(1,0,0))).times(Mat4.scale([ 1, 1, 2]));
+        this.transforms[head]           = Mat4.translation([ 0, 4, 0]).times(this.transforms[head]);
+        this.transforms[left_eyebrow]   = Mat4.translation([ 0, 4, 0]).times(this.transforms[left_eyebrow]);
+        this.transforms[right_eyebrow]  = Mat4.translation([ 0, 4, 0]).times(this.transforms[right_eyebrow]);
+        this.transforms[left_eye]       = Mat4.translation([ 0, 4, 0]).times(this.transforms[left_eye]);
+        this.transforms[right_eye]      = Mat4.translation([ 0, 4, 0]).times(this.transforms[right_eye]);
+        this.transforms[top_beak]       = Mat4.translation([ 0, 4, 0]).times(this.transforms[top_beak]);
+        this.transforms[bottom_beak]    = Mat4.translation([ 0, 4, 0]).times(this.transforms[bottom_beak]);
+
+    }
+
+    attack = () => {
+        let t_frames = 100;
+        if (this.state.frameNumber == 0)
+            this.state.frameNumber = t_frames;
+
+        let tag = '_' + this.constructor.name + this.stats.goose_id;
+        let head = 'head' + tag;
+        let left_eyebrow = 'left_eyebrow' + tag;
+        let right_eyebrow = 'right_eyebrow' + tag;
+        let left_eye = 'left_eye' + tag;
+        let right_eye = 'right_eye' + tag;
+        let top_beak = 'top_beak' + tag;
+        let bottom_beak = 'bottom_beak' + tag;
+        let neck = 'neck' + tag;
+        let body = 'body' + tag;
+        let left_wing = 'left_wing' + tag;
+        let right_wing = 'right_wing' + tag;
+
+        let head_down_transform;
+        let head_up_transform;
         
-        // shape -> current transform, need to initialize
-        this.transforms = {
-            head:       Mat4.identity(),
-            left_eye:   Mat4.identity(),
-            right_eye:  Mat4.identity(),
-            beak:       Mat4.identity(),
-            neck:       Mat4.identity(),
-            left_wing:  Mat4.identity(),
-            body:       Mat4.identity(),
-            right_wing: Mat4.identity(),
-            left_leg:   Mat4.identity(),
-            right_leg:  Mat4.identity(),
-            left_foot:  Mat4.identity(),
-            right_foot: Mat4.identity(),
-        };
+        let neck_down_transform = Mat4.translation([-0.4, -4, 0]) // move neck independently
+            .times(Mat4.rotation(-Math.PI / t_frames / 2, Vec.of(0,0,1)))
+            .times(Mat4.translation([0.4,4,0]));
+        let neck_up_transform = Mat4.translation([-0.4, -4, 0]) // move neck independently
+            .times(Mat4.rotation(Math.PI / t_frames / 2, Vec.of(0,0,1)))
+            .times(Mat4.translation([0.4,4,0]));
+            
+        let body_down_transform = Mat4.translation([-4,-6.75, 0]) // move neck with body
+            .times(Mat4.rotation(-Math.PI / t_frames / 3, Vec.of(0,0,1)))
+            .times(Mat4.translation([4,6.75,0]));
+        let body_up_transform = Mat4.translation([-4,-6.75, 0]) // move neck with body
+            .times(Mat4.rotation(Math.PI / t_frames / 3, Vec.of(0,0,1)))
+            .times(Mat4.translation([4,6.75,0]));
+
+        if (this.state.frameNumber > t_frames/2 + t_frames * 0.1) {
+            this.transforms[head] = body_down_transform
+                .times(neck_down_transform)
+                .times(this.transforms[head]);
+                
+            this.transforms[left_eyebrow] = body_down_transform
+                .times(neck_down_transform)
+                .times(this.transforms[left_eyebrow]);
+                
+            this.transforms[right_eyebrow] = body_down_transform
+                .times(neck_down_transform)
+                .times(this.transforms[right_eyebrow]);
+                
+            this.transforms[left_eye] = body_down_transform
+                .times(neck_down_transform)
+                .times(this.transforms[left_eye]);
+                
+            this.transforms[right_eye] = body_down_transform
+                .times(neck_down_transform)
+                .times(this.transforms[right_eye]);
+                
+            this.transforms[top_beak] = body_down_transform
+                .times(neck_down_transform)
+                .times(this.transforms[top_beak]);
+                
+            this.transforms[bottom_beak] = body_down_transform
+                .times(neck_down_transform)
+                .times(this.transforms[bottom_beak]);
+                
+            this.transforms[left_wing] = body_down_transform
+                .times(this.transforms[left_wing]);    
+                
+            this.transforms[neck] = Mat4.identity()
+                .times(body_down_transform)
+                .times(neck_down_transform)
+                .times(this.transforms[neck]);
+            
+            this.transforms[body] = body_down_transform
+                .times(this.transforms[body]);
+
+            this.transforms[right_wing] = body_down_transform
+                .times(this.transforms[right_wing]);  
+        }
+        else if (this.state.frameNumber > t_frames/2 - t_frames * 0.1) {
+            ;
+        }
+        else {
+            this.transforms[head] = neck_up_transform
+                .times(body_up_transform)
+                .times(this.transforms[head]);
+
+            this.transforms[left_eyebrow] = neck_up_transform
+                .times(body_up_transform)
+                .times(this.transforms[left_eyebrow]);
+
+            this.transforms[right_eyebrow] = neck_up_transform
+                .times(body_up_transform)
+                .times(this.transforms[right_eyebrow]);
+                
+            this.transforms[left_eye] = neck_up_transform
+                .times(body_up_transform)
+                .times(this.transforms[left_eye]);
+                
+            this.transforms[right_eye] = neck_up_transform
+                .times(body_up_transform)
+                .times(this.transforms[right_eye]);
+                
+            this.transforms[top_beak] = neck_up_transform
+                .times(body_up_transform)
+                .times(this.transforms[top_beak]);
+                
+            this.transforms[bottom_beak] = neck_up_transform
+                .times(body_up_transform)
+                .times(this.transforms[bottom_beak]);
+             
+            this.transforms[neck] = neck_up_transform
+                .times(body_up_transform)
+                .times(this.transforms[neck]);
+                
+            this.transforms[left_wing] = body_up_transform
+                .times(this.transforms[left_wing]);    
+
+            this.transforms[body] = body_up_transform
+                .times(this.transforms[body]);
+
+            this.transforms[right_wing] = body_up_transform
+                .times(this.transforms[right_wing]);  
+        }
+
+        this.state.frameNumber--;
+        if (this.state.frameNumber == 0)
+            this.state.animating = false;
     }
 }
